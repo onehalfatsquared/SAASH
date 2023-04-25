522d55d54478c24c1e6b2bae6e6906fd952e62d1
Anthony <anthonytrubiano@gmail.com>
2023-04-25 13:20:46 -0400

    added scipy as a requirement for SAASH. Its quaternion library will be needed to init simulations in stored states

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import find_packages, setup
 setup(
     name='SAASH',
     packages=find_packages(include=["SAASH","SAASH.structure","SAASH.util"]),
-    version='0.1.0',
+    version='0.2.0',
     description='Self-Assembly Analysis Script for HOOMD',
     author='onehalfatsquared',
     license='MIT',
     test_suite='testing',
-    install_requires=['numpy','pandas','gsd']
+    install_requires=['numpy','pandas','gsd','scipy']
 )
