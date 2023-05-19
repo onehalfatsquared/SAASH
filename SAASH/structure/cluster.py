f2e7d8313dc1801b05fdace0fd7668d63970d8a8
onehalfatsquared <anthonytrubiano@gmail.com>
2023-05-19 15:58:41 -0400

    fixed sum issue in some vrsions of python

--- SAASH/structure/cluster.py
+++ SAASH/structure/cluster.py
@@ -505,7 +505,7 @@ class ClusterInfo:
         if self.__is_dead or self.__is_absorbed:
             final_frame = int(self.__observer.get_final_frame()/self.__observer.get_frame_jump())
             L = [0] * (final_frame - self.__death_frame)
-            time_series = sum(time_series, L, [])
+            time_series = sum([time_series, L], [])
 
         return np.array(time_series)
 
