52e6914e2bf28bb8deb80c7f5b0d73d32631cc19
onehalfatsquared <44390083+onehalfatsquared@users.noreply.github.com>
2024-06-04 11:37:01 -0400

    Update analysis_example.py
    Updated the example with more common options for the analysis

--- examples/analysis_example.py
+++ examples/analysis_example.py
@@ -16,14 +16,20 @@ def setup_observer(gsd_file, run_type, observables = None, jump=1):
     #init an observables with the file and run type
     observer = obs.Observer(gsd_file, run_type, jump = jump)
 
-    #for cluster runs, set up all the desired observables
+    #change the start and end frames if desired 
+    # observer.set_first_frame(100)
+    # observer.set_final_frame(400)
+
+    #command to report detailed information about microstates of a given size
+    #observer.set_focus_list([2,3,5,6,10,15,20,30,50,60])
+
+    #command to manually set a cutoff distance for neighborgrid (too small results in never finding neighbors)
+    #observer.set_ngrid_cutoff(3.0)
+
+    #for cluster runs, set up all the desired observables to track during cluster lifetimes
     if run_type == 'cluster' and observables is not None:
         for observable in observables:
             observer.add_observable(observable)
-
-    #change the start and end frames
-    # observer.set_first_frame(100)
-    # observer.set_final_frame(400)
 
     return observer
 
